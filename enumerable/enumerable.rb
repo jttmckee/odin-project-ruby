7e333ff4a98cf44932d6f021f27874980bffd65f
jttmckee <wp.jason@hotmail.co.uk>
2017-07-31 13:17:29 +0100

    my_map, my_inject, multiply_els alld added and Enumerable exercist completed

--- enumerable/enumerable.rb
+++ enumerable/enumerable.rb
@@ -76,7 +76,7 @@ module Enumerable
       if target == nil
         return self.size
       else
-        return self.my_select {|item| item == target }.size 
+        return self.my_select {|item| item == target }.size
       end
     else
       test = self.my_select { |key,value| yield(key,value) } if self.class == Hash
@@ -84,6 +84,42 @@ module Enumerable
       return test.size
     end
   end
+
+  def my_map(a_proc=nil)
+    results = []
+    unless a_proc && a_proc.class == Proc
+      self.my_each { |item,item2| results << yield(item, item2)}
+      return results
+    else
+      self.my_each { |item, item2| results << a_proc.call(item,item2)}
+      return results
+    end
+  end
+
+  def my_inject(startValue = nil)
+    memo =  (startValue ?   startValue : self.first)
+
+    self.my_each { |item| memo = yield(memo,item) } if self.class == Array || self.class == Range
+
+    if self.class == Hash
+      self.my_each { |key, value|
+        object = key, value
+        memo = yield(memo, object)
+
+      }
+
+    end
+
+    return memo
+
+  end
+
+  def multiply_els
+    return self.my_inject {|memo, item| memo*item} if self.class == Array || self.class == Range
+    return self.values.multiply_els if self.class == Hash
+  end
+
+
 
 end
 
@@ -182,17 +218,57 @@ end
 # puts (0...10).none? { |item| item % 2 == 0 }
 # puts (1...10).none? { |item| item % 13 == 0 }
 #Tests for my_count
-puts "my_count"
-puts ({dogs: 0, cats:2, fish:3}.my_count(2))
-puts ({apes: 0}.my_count)
-puts ({dogs: 0, cats:2, fish:3}.my_count {|key, value| value > 1})
-puts ([0,2,6,4,6,7].my_count(6))
-puts ([0,2,6,4,6,7].my_count {|value| value > 3})
-puts ((2..10).my_count{|number| number >= 4})
-puts "count"
-puts ({dogs: 0, cats:2, fish:3}.count(2))
-puts ({apes: 0}.count)
-puts ({dogs: 0, cats:2, fish:3}.count {|key, value| value > 1})
-puts ([0,2,6,4,6,7].count(6))
-puts ([0,2,6,4,6,7].count {|value| value > 3})
-puts ((2..10).count{|number| number >= 4})
+# puts "my_count"
+# puts ({dogs: 0, cats:2, fish:3}.my_count(2))
+# puts ({apes: 0}.my_count)
+# puts ({dogs: 0, cats:2, fish:3}.my_count {|key, value| value > 1})
+# puts ([0,2,6,4,6,7].my_count(6))
+# puts ([0,2,6,4,6,7].my_count {|value| value > 3})
+# puts ((2..10).my_count{|number| number >= 4})
+# puts "count"
+# puts ({dogs: 0, cats:2, fish:3}.count(2))
+# puts ({apes: 0}.count)
+# puts ({dogs: 0, cats:2, fish:3}.count {|key, value| value > 1})
+# puts ([0,2,6,4,6,7].count(6))
+# puts ([0,2,6,4,6,7].count {|value| value > 3})
+# puts ((2..10).count{|number| number >= 4})
+# tests for my_map
+# puts "my_map"
+# puts({dogs:2, cats:3}.my_map { |key, value| value +=3 })
+# puts([0,1,2,3,4].my_map {|number| number * 4})
+# puts((2..10).my_map {|number| number * 4})
+# # puts "map"
+# # puts({dogs:2, cats:3}.map { |key, value| value +=3 })
+# # puts([0,1,2,3,4].map {|number| number * 4})
+# # puts((2..10).map {|number| number * 4})
+# puts "my_map with proc"
+# puts({dogs:2, cats:3}.my_map (Proc.new { |key, value| value +=3}))
+# puts([0,1,2,3,4].my_map (Proc.new {|number| number * 4 }))
+# puts((2..10).my_map (Proc.new {|number| number * 4}))
+
+#Tests my_inject
+# puts "my_inject"
+# puts([0,1,2,3].my_inject {|memo, item| memo + item})
+# puts([0,1,2,3].my_inject(10) {|memo, item| memo - item})
+# puts ((1..10).my_inject {|memo, item| memo * item})
+# puts  ({red: 0, blue: 2, green: 3}.inject({}) do |memo, obj|
+#   key, value = obj
+#   memo[key] = value if value >= 2
+#   memo
+# end
+#   )
+# puts "inject"
+# puts([0,1,2,3].inject {|memo, item| memo + item})
+# puts([0,1,2,3].inject(10) {|memo, item| memo - item})
+# puts ((1..10).inject {|memo, item| memo * item})
+# puts  ({red: 0, blue: 2, green: 3}.inject({}) do |memo, obj|
+#   key, value = obj
+#   memo[key] = value if value >= 2
+#   memo
+# end
+#   )
+#Test multiply_els
+# puts "multiply_els"
+# puts ([1,2,3].multiply_els)
+# puts ((1..10).multiply_els)
+# puts ({red: 2, green: 3, blue: 4}.multiply_els)
