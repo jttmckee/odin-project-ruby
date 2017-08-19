fd48994df8faafee079815617297e41b25f36332
jttmckee <wp.jason@hotmail.co.uk>
2017-08-19 22:45:27 +0100

    Event Manager week day frequency finished, project complete

--- event_manager/lib/event_date.rb
+++ event_manager/lib/event_date.rb
@@ -13,3 +13,14 @@ hours_freq = hours.inject({}) do |memo, hour|
   memo
 end
 puts hours_freq
+
+contents = CSV.open "event_attendees.csv", headers:true, header_converters: :symbol
+
+days_freq = contents.inject({}) do |memo, row|
+  wday = DateTime.strptime(row[:regdate], '%m/%d/%Y %H:%M').wday
+  memo.tap { |h| h.store(wday, memo[wday] ? memo[wday] + 1 : 1 )}
+
+end
+
+
+puts days_freq
