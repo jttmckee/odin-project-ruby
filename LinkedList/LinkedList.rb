9b894a69a4c8a8af6246ee7f2dc0e4dda40eea1e
jttmckee <wp.jason@hotmail.co.uk>
2017-09-04 21:34:42 +0100

    added prepend, size, head, tail, at, pop to LinkedList

--- LinkedList/LinkedList.rb
+++ LinkedList/LinkedList.rb
@@ -4,7 +4,7 @@ class LinkedList
   attr_accessor :head_node
 
   def append(node)
-    unless node.is_a? Node then node = Node.new(node) end
+    node = make_node(node)
     if @head_node
       @head_node.get_tail_node.next_node = node
     else
@@ -12,5 +12,48 @@ class LinkedList
     end
   end
 
+  def prepend(node)
+    node = make_node(node)
+    if @head_node
+      node.next_node = @head_node
+      @head_node = node
+    else
+      @head_node = node
+    end
+  end
+
+  def size
+    @head_node.count
+  end
+
+  def head
+    @head_node
+  end
+
+  def tail
+    @head_node ? @head_node.get_tail_node : nil
+  end
+
+  def at(index)
+    @head_node ? @head_node.at(index) : nil
+  end
+
+  def pop
+    unless @head_node then return nil end
+    if @head_node.next_node
+      @head_node.get_penultimate_node.remove_instance_variable(:@next_node)
+    else
+      remove_instance_variable(:@head_node)
+    end
+
+  end
+
+
+  private
+  def make_node(object)
+   node = (object.is_a? Node) ? object : Node.new(object)
+   node.next_node = nil
+   node
+  end
 
 end
--- LinkedList/Node.rb
+++ LinkedList/Node.rb
@@ -9,4 +9,19 @@ class Node
     @next_node ? @next_node.get_tail_node : self
   end
 
+  def count
+    @next_node ? @next_node.count + 1 : 1
+  end
+
+  def get_penultimate_node
+    @next_node.next_node ? @next_node.get_penultimate_node : self
+  end
+
+  def at(index)
+    if index == 0
+       self
+    else
+      @next_node ?  @next_node.at(index - 1) : nil
+    end
+  end
 end
